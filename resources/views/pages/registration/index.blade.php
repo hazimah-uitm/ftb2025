fab562a2aafccc878cd88d6fe9795df9ea416f60
hazimah-uitm <hazimahpethie@uitm.edu.my>
2025-07-21 00:38:56 +0800

    add deleted

--- resources/views/pages/registration/index.blade.php
+++ resources/views/pages/registration/index.blade.php
@@ -98,14 +98,14 @@
                                                     Semak
                                                 </button>
                                             @endif
-                                            {{-- <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal"
-                                                data-bs-target="#deleteModal{{ $registration->id }}" title="Padam">
-                                                <i class="bx bx-trash"></i>
-                                            </button> --}}
                                             <a href="{{ route('registration.pdf', $registration->id) }}"
                                                 class="btn btn-warning btn-sm" target="_blank">
                                                 <i class="bx bxs-file-pdf"></i> PDF
                                             </a>
+                                            <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal"
+                                                data-bs-target="#deleteModal{{ $registration->id }}" title="Padam">
+                                                <i class="bx bx-trash"></i>
+                                            </button>
                                         @endhasanyrole
                                     </td>
                                 </tr>
